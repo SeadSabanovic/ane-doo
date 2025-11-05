dbcf39b3476d10c78c54c5ae2ab0802f0d53f4be
Sead Sabanovic <sead.sabanovic97@gmail.com>
2025-11-05 21:07:44 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <LenisScrollProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased select-none`}
           suppressHydrationWarning
         >
           <Navigation />
