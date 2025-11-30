57abec6bcd6c01bf6980e6b2eb5c868d5d6f4778
Sead Sabanovic <sead.sabanovic97@gmail.com>
2025-11-30 15:15:57 +0100

    Add Magnetic Effect To Interactive Hover Button

--- src/components/ui/interactive-hover-button.tsx
+++ src/components/ui/interactive-hover-button.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import * as React from "react";
+import { motion, type HTMLMotionProps } from "motion/react";
 import { ArrowRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
+interface Position {
+  x: number;
+  y: number;
+}
+
 interface InteractiveHoverButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends Omit<HTMLMotionProps<"button">, "children"> {
+  children: React.ReactNode;
   variant?: "dark" | "light";
 }
 
@@ -13,10 +23,37 @@ export function InteractiveHoverButton({
   variant = "dark",
   ...props
 }: InteractiveHoverButtonProps) {
+  const ref = React.useRef<HTMLButtonElement>(null);
+  const [position, setPosition] = React.useState<Position>({ x: 0, y: 0 });
+
+  const handleMouse = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (ref.current) {
+      const { clientX, clientY } = e;
+      const { height, width, left, top } = ref.current.getBoundingClientRect();
+      const middleX = clientX - (left + width / 2);
+      const middleY = clientY - (top + height / 2);
+      setPosition({ x: middleX, y: middleY });
+    }
+  };
+
+  const reset = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = position;
+
   return (
-    <button
+    <motion.button
+      ref={ref}
+      onMouseMove={handleMouse}
+      onMouseLeave={reset}
+      animate={{ x, y }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+      whileTap={{
+        scale: 0.95,
+      }}
       className={cn(
-        "group bg-primary text-primary-foreground relative w-auto cursor-pointer overflow-hidden rounded-md px-6 py-2 text-center font-semibold",
+        "group bg-primary text-primary-foreground relative w-auto cursor-pointer overflow-hidden rounded-md px-6 py-2 text-center font-semibold transition-none",
         variant === "dark" && "bg-primary text-primary-foreground",
         variant === "light" && "bg-background text-primary border",
         className
@@ -38,6 +75,6 @@ export function InteractiveHoverButton({
         <span>{children}</span>
         <ArrowRight />
       </div>
-    </button>
+    </motion.button>
   );
 }
