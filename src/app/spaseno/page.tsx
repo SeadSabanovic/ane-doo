1b5ad3f0440b7abd627345736196f2033c45b4d6
Sead Sabanovic <sead.sabanovic97@gmail.com>
2025-12-10 22:38:45 +0100

    Update page.tsx

--- src/app/spaseno/page.tsx
+++ src/app/spaseno/page.tsx
@@ -16,18 +16,66 @@ export default function FavoritesPage() {
   const favorites = [
     {
       id: 1,
-      name: "Basic T-Shirt",
-      price: 29.99,
-      image: "/images/product/shirt.png",
-      slug: "basic-t-shirt",
+      name: "Product 1",
+      price: 16.99,
+      image:
+        "https://i.pinimg.com/736x/d7/5b/4f/d75b4f080a55b0500799077eb37989a3.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 17.99,
+      image:
+        "https://i.pinimg.com/1200x/82/6b/35/826b3584f6cdeb75dcbf1e2801313521.jpg",
       link: "/shop/basic-t-shirt",
     },
     {
       id: 3,
-      name: "Denim Jacket",
-      price: 89.99,
-      image: "/images/product/shirt.png",
-      slug: "denim-jacket",
+      name: "Product 3",
+      price: 18.99,
+      image:
+        "https://i.pinimg.com/736x/d8/6c/a7/d86ca7b2f915885bc5ef7d3b1e6cd4ec.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 19.99,
+      image:
+        "https://i.pinimg.com/736x/7e/c1/b6/7ec1b6940782133e1a9d04f2db1e2134.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 20.99,
+      image:
+        "https://i.pinimg.com/736x/fe/bc/b4/febcb44882494610c8c4b1f5b55ea74b.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      price: 21.99,
+      image:
+        "https://i.pinimg.com/1200x/36/83/8f/36838f6fd4d878649c5eab015f591aff.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 7,
+      name: "Product 7",
+      price: 22.99,
+      image:
+        "https://i.pinimg.com/736x/27/1a/1d/271a1d30b5c96cf9bbeb4a9aaf861548.jpg",
+      link: "/shop/basic-t-shirt",
+    },
+    {
+      id: 8,
+      name: "Product 8",
+      price: 23.99,
+      image:
+        "https://i.pinimg.com/1200x/b2/4b/fc/b24bfcec26d2772701f88e06192d1d09.jpg",
       link: "/shop/basic-t-shirt",
     },
   ];
