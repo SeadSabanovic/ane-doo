7c816ec77681b71dc494d5997d5a1d8f9ee9a41f
Sead Sabanovic <sead.sabanovic97@gmail.com>
2026-01-11 20:42:57 +0100

    Update Landing Page

--- src/components/sections/home/hero/hero-carousel.tsx
+++ src/components/sections/home/hero/hero-carousel.tsx
@@ -7,42 +7,42 @@ const heroData = {
   title: "Pouzdan veleprodajni partner",
   description:
     "Porodična firma koja već više od 20 godina spaja tradiciju i veleprodaju odjeće i kućnog tekstila.",
-  image: "https://images.pexels.com/photos/3262937/pexels-photo-3262937.jpeg",
+  image: "https://images.pexels.com/photos/3965545/pexels-photo-3965545.jpeg",
   ctaHref: "/shop",
 };
 
 export default function HeroCarousel() {
   return (
-    <section className="bg-muted/20 overflow-hidden h-full min-h-[calc(85svh-var(--nav-height))] lg:min-h-[calc(75svh-var(--nav-height))] flex flex-col items-center justify-center relative">
-      <Container className="flex flex-1 flex-col justify-end gap-6 h-full w-full relative z-30">
-        <div className="flex flex-col justify-between lg:justify-center items-center lg:items-start p-6 pt-4 lg:pt-6 py-12 h-fit lg:rounded-tr-lg pb-[10svh] ">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-lg font-bold text-center lg:text-left text-background text-shadow-sm">
+    <section className="overflow-hidden h-full min-h-[calc(85svh-var(--nav-height))] lg:min-h-[calc(75svh-var(--nav-height))] flex flex-col items-center justify-center relative sm:py-4">
+      <Container className="flex flex-1 flex-col justify-end gap-6 h-full w-full z-30 relative sm:rounded-lg overflow-hidden">
+        <div className="flex flex-col justify-between lg:justify-center items-center lg:items-start p-6 pt-4 lg:pt-6 py-12 h-fit lg:rounded-tr-lg pb-[10svh] z-30">
+          <div className="flex flex-col gap-2 items-center w-full">
+            <h1 className="text-lg font-bold text-center text-background text-shadow-sm">
               ANE D.O.O.
             </h1>
-            <h2 className="text-6xl font-bold text-center lg:text-8xl lg:text-left text-background text-shadow-sm">
+            <h2 className="text-5xl font-bold lg:text-6xl text-background text-shadow-sm text-center">
               {heroData.title}
             </h2>
-            <p className="mt-4 text-center text-lg lg:text-left md:text-2xl text-shadow-sm text-background">
+            <p className="mt-4 text-center text-lg md:text-2xl text-shadow-sm text-background max-w-2xl">
               {heroData.description}
             </p>
+            <Link href={heroData.ctaHref}>
+              <InteractiveHoverButton className="mt-8 w-fit text-center">
+                Pogledaj
+              </InteractiveHoverButton>
+            </Link>
           </div>
-          <Link href={heroData.ctaHref}>
-            <InteractiveHoverButton className="mt-8 w-fit text-center lg:text-left">
-              Pogledaj
-            </InteractiveHoverButton>
-          </Link>
         </div>
+        <AnimatedImage
+          src={heroData.image}
+          alt={heroData.title}
+          width={1600}
+          height={900}
+          className="object-cover absolute top-0 left-0 w-full h-full z-10"
+          priority
+        />
+        <div className="absolute bottom-0 left-0 w-full h-full z-20 bg-linear-to-t from-primary/50 to-primary/0" />
       </Container>
-      <AnimatedImage
-        src={heroData.image}
-        alt={heroData.title}
-        width={1600}
-        height={900}
-        className="object-cover absolute top-0 left-0 w-full h-full z-10"
-        priority
-      />
-      <div className="absolute bottom-0 left-0 w-full h-full z-20 bg-linear-to-t from-secondary to-secondary-muted/0" />
     </section>
   );
 }
--- src/components/sections/home/hero/hero-feature.tsx
+++ src/components/sections/home/hero/hero-feature.tsx
@@ -2,34 +2,37 @@ import { Award, BadgeCheck, Boxes, Store, Tag, Truck } from "lucide-react";
 import { Marquee } from "@/components/ui/marquee";
 import { cn } from "@/lib/utils";
 
+const ICON_SIZE = 36;
+const ICON_STROKE_WIDTH = 1;
+
 const featureData = [
   {
-    icon: <Store />,
+    icon: <Store size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "Veleprodaja i maloprodaja",
     description: "Mogućnost kupovine manjih količina",
   },
   {
-    icon: <Tag />,
+    icon: <Tag size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "Veliki izbor artikala",
     description: "Široka ponuda odjeće i kućnog tekstila",
   },
   {
-    icon: <Truck />,
+    icon: <Truck size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "Brza isporuka",
     description: "Pouzdana dostava širom BiH",
   },
   {
-    icon: <BadgeCheck />,
+    icon: <BadgeCheck size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "Provjeren kvalitet",
     description: "Saradnja sa dugogodišnjim dobavljačima",
   },
   {
-    icon: <Award />,
+    icon: <Award size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "20+ godina tradicije",
     description: "Porodična firma sa dugogodišnjim povjerenjem",
   },
   {
-    icon: <Boxes />,
+    icon: <Boxes size={ICON_SIZE} strokeWidth={ICON_STROKE_WIDTH} />,
     title: "Uvijek dostupna roba",
     description: "Stalan lager najtraženijih artikala",
   },
@@ -45,14 +48,15 @@ const FeatureCard = ({
   description: string;
 }) => {
   return (
-    <div className={cn("flex items-center gap-4 flex-col py-20 px-5 shrink-0")}>
-      <div className="size-10 bg-accent/10 rounded-full flex items-center justify-center text-accent">
+    <div className={cn("flex items-center gap-4 flex-col py-10 px-5 shrink-0")}>
+      <div className="size-16 flex items-center justify-center text-card-foreground relative">
         {icon}
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 size-[60%] bg-accent/40 rounded-full -z-10" />
       </div>
 
       <div className="text-center">
-        <h3 className="font-bold text-lg text-background">{title}</h3>
-        <p className="text-md text-muted">{description}</p>
+        <h3 className="font-bold text-lg">{title}</h3>
+        <p className="text-md">{description}</p>
       </div>
     </div>
   );
@@ -60,8 +64,8 @@ const FeatureCard = ({
 
 const HeroFeature = () => {
   return (
-    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden bg-linear-to-r from-primary to-card-foreground">
-      <Marquee pauseOnHover className="[--duration:20s]">
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+      <Marquee className="[--duration:20s] md:[--duration:40s]">
         {featureData.map((item, index) => (
           <FeatureCard
             key={`feature-${index}`}
