cc900385f64bc872789e9e088954967a99188565
Sead Sabanovic <sead.sabanovic97@gmail.com>
2026-01-11 18:54:14 +0100

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,16 +4,16 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-linear-to-r from-secondary to-secondary-muted">
+    <footer className="bg-linear-to-r from-primary to-card-foreground text-secondary-foreground">
       <Container>
         {/* <!-- footer menu start --> */}
         <div className="flex flex-wrap flex-col md:flex-row xl:flex-nowrap gap-10 xl:gap-19 xl:justify-between py-12 xl:py-24">
           <div className="flex-1">
-            <h2 className="mb-4 text-secondary-foreground font-medium text-xl">
+            <h2 className="mb-4 text-accent font-medium text-xl">
               Informacije i podrška
             </h2>
 
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-2 text-primary-foreground">
               <li>
                 <a href="#" className="w-fit">
                   info@ane-doo.com
@@ -41,11 +41,11 @@ const Footer = () => {
           </div>
 
           <div>
-            <h2 className="mb-4 text-secondary-foreground font-medium text-xl">
+            <h2 className="mb-4 text-accent font-medium text-xl">
               Linkovi
             </h2>
 
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-2 text-primary-foreground">
               <li>
                 <a className="ease-out duration-200 hover:underline" href="#">
                   Početna
@@ -70,11 +70,11 @@ const Footer = () => {
           </div>
 
           <div>
-            <h2 className="mb-4 text-secondary-foreground font-medium text-xl">
+            <h2 className="mb-4 text-accent font-medium text-xl">
               ANE D.O.O.
             </h2>
 
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-2 text-primary-foreground">
               <li>
                 <a className="ease-out duration-200 hover:underline" href="#">
                   Politika privatnosti
@@ -100,7 +100,7 @@ const Footer = () => {
         </div>
       </Container>
 
-      <div className="py-5 xl:py-7.5 bg-linear-to-r from-primary to-card-foreground text-accent">
+      <div className="py-5 xl:py-7.5 bg-accent text-accent-foreground font-medium">
         <Container>
           <p>&copy; 2000 - {year} ANE D.O.O. Sva prava zadržana.</p>
         </Container>
