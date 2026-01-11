d76179e531e6f418a78de6d3d96227c4f7305bce
Sead Sabanovic <sead.sabanovic97@gmail.com>
2026-01-11 20:11:07 +0100

    Update hero-feature.tsx

--- src/components/sections/home/hero/hero-feature.tsx
+++ src/components/sections/home/hero/hero-feature.tsx
@@ -1,4 +1,3 @@
-import Container from "@/components/layout/container";
 import { Award, BadgeCheck, Boxes, Store, Tag, Truck } from "lucide-react";
 import { Marquee } from "@/components/ui/marquee";
 import { cn } from "@/lib/utils";
@@ -60,47 +59,19 @@ const FeatureCard = ({
 };
 
 const HeroFeature = () => {
-  const firstRow = featureData;
-
   return (
-    <>
-      {/* Mobile/Tablet - Marquee Animation */}
-      <div className="lg:hidden relative flex w-full flex-col items-center justify-center overflow-hidden bg-linear-to-r from-primary to-card-foreground">
-        <Marquee pauseOnHover className="[--duration:20s]">
-          {firstRow.map((item, index) => (
-            <FeatureCard
-              key={`feature-1-${index}`}
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
-            />
-          ))}
-        </Marquee>
-      </div>
-
-      {/* Desktop - Grid Layout */}
-      <div className="bg-linear-to-r from-primary to-card-foreground py-10 hidden lg:block">
-        <Container className="lg:grid lg:grid-cols-3 rounded-b-md mx-auto overflow-hidden">
-          {featureData.map((item, key) => (
-            <div
-              className={"flex items-center gap-4 flex-col xl:flex-1 py-6 px-5"}
-              key={key}
-            >
-              <div className="size-10 bg-accent/10 rounded-full flex items-center justify-center text-accent">
-                {item.icon && item.icon}
-              </div>
-
-              <div className="text-center">
-                <h3 className="font-bold text-lg text-background">
-                  {item.title}
-                </h3>
-                <p className="text-md text-muted">{item.description}</p>
-              </div>
-            </div>
-          ))}
-        </Container>
-      </div>
-    </>
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden bg-linear-to-r from-primary to-card-foreground">
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {featureData.map((item, index) => (
+          <FeatureCard
+            key={`feature-${index}`}
+            icon={item.icon}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </Marquee>
+    </div>
   );
 };
 
